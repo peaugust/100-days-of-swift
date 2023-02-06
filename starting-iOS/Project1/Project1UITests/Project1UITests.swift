611d9595d0d085a4076f097d1c9522c4e90da2b6
Pedro <peafreddi@gmail.com>
2023-02-06 16:22:51 -0300

    Add UITests to Project1

--- starting-iOS/Project1/Project1UITests/Project1UITests.swift
+++ starting-iOS/Project1/Project1UITests/Project1UITests.swift
@@ -21,21 +21,26 @@ final class Project1UITests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    
+    func testTableViewDisplay() throws {
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let tableView = app.tables.element
+        
+        XCTAssertTrue(tableView.exists)
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    func testImageDisplay() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        let tableView = app.tables.element
+        let firstCell = tableView.cells.element(boundBy: 0)
+        firstCell.tap()
+    
+        let presentedView = app.otherElements["ImageDetails"]
+        XCTAssertTrue(presentedView.exists, "ImageDetails view not found")
+        XCTAssertEqual(presentedView.images.element.identifier, "ImageView" , "ImageView not found")
     }
 }
